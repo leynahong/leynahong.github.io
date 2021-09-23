9b56ee3c842cf82d58b39440e94f8e0aa0191587
leynahong <90758728+leynahong@users.noreply.github.com>
2021-09-22 17:23:15 -0700

    Create 0922.py

---
+++ 0922.py/0922.py
@@ -0,0 +1,91 @@
+import math
+
+################################################################################
+# lists
+################################################################################
+'''
+def largest(xs):
+    
+    Return the largest element in the input list.
+    If the list has no elements, return None.
+    HINT:
+    There are three ways to solve this problem:
+    1. use a for loop + if statement
+    2. sort the list and use list subscripting
+    3. use a built-in function
+    >>> largest([1,2,3])
+    3
+    >>> largest([99,-56,80,100,90])
+    100
+    >>> largest(list(range(0,100)))
+    99
+    >>> largest([10])
+    10
+    >>> largest([])
+    '''
+'''
+if len(xs)==0:
+    return None
+biggest = 0
+for x in xs:
+    if x > biggest:
+        biggest = x
+return biggest
+    
+if len(xs) == 0:
+    return None
+xs.sort()
+return xs[-1]
+
+if len(xs) == 0:
+    return None
+return max(xs)
+'''
+
+def largest_index(xs):
+    '''
+    Return the index of the largest element in the input list.
+    If the list has no elements, return None.
+    HINT:
+    The sorting/built-in function approach will not work on this function.
+    >>> largest_index([1,2,3])
+    2
+    >>> largest_index([99,-56,80,100,90])
+    3
+    >>> largest_index(list(range(0,100)))
+    99
+    >>> largest_index([10])
+    0
+    >>> largest_index([])
+    '''
+    if len(xs) == 0:
+        return None
+    biggest = 0
+    for i in range (len(xs)):
+        x = xs[i]
+        if x > biggest:
+            biggest = x
+            biggest_index = i
+    return biggest_index
+
+def filter_odd(xs):
+    '''
+    Return a list with all the odd elements removed.
+    HINT:
+    Use the accumulator pattern with a for loop.
+    >>> filter_odd([1,3,5])
+    []
+    >>> filter_odd([2,4,6])
+    [2, 4, 6]
+    >>> filter_odd([4,5,6,7])
+    [4, 6]
+    >>> filter_odd([20,13,4,16,8,19,10])
+    [20, 4, 16, 8, 10]
+    '''
+
+    accumulator = []
+    for x in xs:
+        if x%2 == 0 :
+            accumulator.append(x)
+        # append to the accumulator if our condition is satisfied
+    return accumulator 
